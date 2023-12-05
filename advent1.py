6858555d9953ecab085ea52ca88d5804ee03182f
aamilonas <angelomilonas1@gmail.com>
2023-12-05 13:19:44 -0500

    Correct answer achieved for a1

--- advent1.py
+++ advent1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 file = open("av1.txt","r")
 
 lines = file.readlines()
@@ -5,24 +7,27 @@ lines = file.readlines()
 allnums = []
 
 for line in lines:
-   l, r = 0, len(line)-1
-   val1, val2 = 69, 69
+    l, r = 0, len(line)-1
+    val1, val2 = 69, 69
 
-   while l!=r:
+    while l<len(line)-1:
         if line[l].isdigit():
            val1 = line[l]
+        else:
+            l+=1
         if line[r].isdigit():
             val2=line[r]
-        l+=1
-        r-=1
+        else:
+            r-=1
         if val1!=69 and val2!=69:
-            break
+           break
         
-   if val1!= 69 and val2!=69:
+    if val1!= 69 and val2!=69:
         oval = str(val1)+str(val2)
         allnums.append(int(oval))
 
 print(allnums)
 
+print(np.sum(allnums))
 
 file.close()
